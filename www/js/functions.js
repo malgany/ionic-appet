85d26831bb6bfbbe9c2b2b9c201693701fafd5c2
malgany <yopsadida@gmail.com>
2017-01-27 23:39:52 -0300

    ini 9.2

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,5 +1,5 @@
 function getImage() {
-    navigator.camera.getPicture(uploadPhoto, function(message) {
+    navigator.camera.getPicture(cortarImagem, function(message) {
         alert('get picture failed');
     }, {
         quality: 100,
@@ -8,46 +8,60 @@ function getImage() {
     });
 }
 
-function uploadPhoto(imageURI) {
-    var options = new FileUploadOptions();
-    options.fileKey = "file";
-    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
-    options.mimeType = "image/jpeg";
-    console.log(options.fileName);
-    var params = new Object();
-    params.value1 = "test";
-    params.value2 = "param";
-    options.params = params;
-    options.chunkedMode = false;
+function cortarImagem(imageURI) {
 
     plugins.crop.promise(imageURI, {quality: 90})
     .then(function success (newPath) {
-        saveImage(newPath);
         $("#pet-image").html('<img src="'+newPath+'" alt="Smiley face" height="100%" width="100%">');
+        $("#pet-image").data("urlimg", newPath);
     })
     .catch(function fail (err) {
 
     })
-    /*var ft = new FileTransfer();
-    ft.upload(imageURI, "http://192.168.1.4/phonegap/upload/upload.php", function(result){
-        console.log(JSON.stringify(result));
-    }, function(error){
-        console.log(JSON.stringify(error));
-    }, options);*/
- }
-
-function saveImage(newPath) {
-    alert(newPath);
-    $.ajax({
-        type: "POST",
-        url: "http://api.tumblr.com/v2/blog/tonyhtml.tumblr.com/post?api_key=jlZbRx7boomg9Cvk6PlckOA4DI38rNzNm5x9GKrQjWWPxd7Ptf",
-        dataType: 'jsonp',
-        data: { type: 'photo', source: newPath },
-        success: function (posts) {
-            alert('ok');
-            //var postings = posts.response.posts;
-            //alert(posts.response);
-            //alert(postings);
-        }
-    });
 }
+
+function transferirImagem() {
+
+    var imageURI = $("#pet-image").data("urlimg");
+
+    if(imageURI == "")
+        return ;
+
+    var params = JSON.stringify($('form').serializeObject());
+
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+    options.mimeType = "image/jpeg";
+    options.params = params;
+    options.chunkedMode = false;
+
+    var ft = new FileTransfer();
+    ft.upload(imageURI, "http://tony-malgany.rhcloud.com/ajax.php", function(result){
+        alert(JSON.stringify(result));
+    }, function(error){
+        alert(JSON.stringify(error));
+    }, options);
+}
+
+function saveCadastroDesaparecido() {
+
+    transferirImagem();
+}
+
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
